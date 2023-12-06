e95c25162b8dd2a59a10c71fe2974549c9af002e
James <jamesfleming.sd@gmail.com>
2023-12-06 15:30:54 -0800

    database update

--- Server/database.sql
+++ Server/database.sql
@@ -1,15 +1,21 @@
 CREATE DATABASE IF NOT EXISTS reshop;
 
-CREATE TABLE user(
+CREATE TABLE IF NOT EXISTS user(
     id SERIAL PRIMARY KEY,
     username VARCHAR(50),
     email VARCHAR(50),
 );
 
-CREATE TABLE post(
+CREATE TABLE IF NOT EXISTS post(
     id SERIAL PRIMARY KEY,
     item_name VARCHAR(50),
     item_bio VARCHAR(255),
     item_value BIGINT,
     created TIME,
+);
+
+CREATE TABLE IF NOT EXISTS message(
+    id SERIAL PRIMARY KEY,
+    sentby INTEGER REFERENCES user(id),
+    sentto INTEGER REFERENCES user(id),
 );
