e93f2f37bec2700cc00a106e7c9d126c7e9c29a5
James <jamesfleming.sd@gmail.com>
2023-12-08 14:52:19 -0800

    simplified

--- Server/database.sql
+++ Server/database.sql
@@ -8,26 +8,21 @@ CREATE TABLE IF NOT EXISTS user(
     region VARCHAR(20),
 );
 
+CREATE TABLE IF NOT EXISTS tags(
+    id SERIAL PRIMARY KEY,
+    tag_title VARCHAR(20) UNIQUE NOT NULL,
+);
+
 CREATE TABLE IF NOT EXISTS post(
     id SERIAL PRIMARY KEY,
     item_name VARCHAR(50),
     item_bio VARCHAR(255),
     item_value BIGINT,
     created TIME DEFAULT CURRENT_TIMESTAMP,
-);
-
-CREATE TABLE IF NOT EXISTS tags(
-    id SERIAL PRIMARY KEY,
-    tag_title VARCHAR(20) UNIQUE NOT NULL,
-);
-
-CREATE TABLE IF NOT EXISTS PostTag(
-    user_id INT references user(id),
-    tag_id INT references tag(id),
-    PRIMARY KEY (user_id, role_id)
+    tags VARCHAR(20)[] REFERENCES tags(id),
 );
 
 CREATE TABLE IF NOT EXISTS message(
     id SERIAL PRIMARY KEY,
-    FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE,
+    user_id INT REFERENCES user(id) ON DELETE CASCADE,
 );
