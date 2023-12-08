bff51b976c9bea16d2f0a8419c9e3e360dc6eef4
James <jamesfleming.sd@gmail.com>
2023-12-08 14:49:35 -0800

    updates to the SQL database schema

--- Server/database.sql
+++ Server/database.sql
@@ -2,9 +2,10 @@ CREATE DATABASE IF NOT EXISTS reshop;
 
 CREATE TABLE IF NOT EXISTS user(
     id SERIAL PRIMARY KEY,
-    usercode VARCHAR(6),
-    username VARCHAR(50),
+    usercode VARCHAR(6) NOT NULL,
+    username VARCHAR(50) NOT NULL,
     email VARCHAR(50),
+    region VARCHAR(20),
 );
 
 CREATE TABLE IF NOT EXISTS post(
@@ -12,11 +13,21 @@ CREATE TABLE IF NOT EXISTS post(
     item_name VARCHAR(50),
     item_bio VARCHAR(255),
     item_value BIGINT,
-    created TIME,
+    created TIME DEFAULT CURRENT_TIMESTAMP,
+);
+
+CREATE TABLE IF NOT EXISTS tags(
+    id SERIAL PRIMARY KEY,
+    tag_title VARCHAR(20) UNIQUE NOT NULL,
+);
+
+CREATE TABLE IF NOT EXISTS PostTag(
+    user_id INT references user(id),
+    tag_id INT references tag(id),
+    PRIMARY KEY (user_id, role_id)
 );
 
 CREATE TABLE IF NOT EXISTS message(
     id SERIAL PRIMARY KEY,
-    sentby INTEGER REFERENCES user(id),
-    sentto INTEGER REFERENCES user(id),
+    FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE,
 );
