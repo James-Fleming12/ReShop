c598af6551c42f5de2ca0369acc6d58c966b590e
James <jamesfleming.sd@gmail.com>
2023-12-31 13:19:35 -0800

    Added Change Information Component

---
+++ Client/src/components/Reactive/changeInfo.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    let responseMessage: string;
+    async function submit(e: SubmitEvent){
+        e.preventDefault();
+        const formData = new FormData(e.currentTarget as HTMLFormElement);
+        const response = await fetch("", {
+            method: "POST",
+            body: formData,
+        });
+        if(response.ok){
+            window.location.href = "/";
+        }
+        const data = await response.json();
+        responseMessage = data.message;
+    }
+</script>
+
+<style>
+
+</style>
+
+<form>
+
+</form>
